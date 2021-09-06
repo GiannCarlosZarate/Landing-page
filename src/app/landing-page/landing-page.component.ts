e201ddf379ba23f554ea9c23f4ba8e45a6f7d51c
giann-zarate <giann.zarate@doctus.com.co>
2021-09-06 08:07:12 -0500

    se agregan los cambios de las paginas

---
+++ src/app/landing-page/landing-page.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+declare var $:any;
+
+@Component({
+  selector: 'app-landing-page',
+  templateUrl: './landing-page.component.html',
+  styleUrls: ['./landing-page.component.scss']
+})
+export class LandingPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+    $( "#btnModal" ).click(function() {
+      $('#exampleModal').modal('show')
+    });
+    
+  }
+
+}
